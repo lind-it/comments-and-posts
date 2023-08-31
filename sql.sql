d820eb448f46bde8cd0f7d7005ec25a00c00ef04
lind-it <58984196+lind-it@users.noreply.github.com>
2023-08-31 15:29:05 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,23 +1,17 @@
---1
-SELECT * FROM users WHERE 1;
---2
-SELECT * FROM users WHERE deleted = 1;
---3
-SELECT * FROM users ORDER BY name ASC; 
---4
-SELECT users.citi_id, cities.id, COUNT(users.name)
-FROM users JOIN cities
-ON users.citi_id = cities.id
-GROUP By cities.id;
---5
-SELECT users.name ||" "|| users.secondName AS name, users.citi_id, cities.id, COUNT(users.name)
-FROM users JOIN cities
-ON users.citi_id = cities.id
-WHERE deleted = 0
-GROUP BY cities.id
-ORDER BY name;
---6
-SELECT users.name ||" "|| users.secondName AS name
-FROM users JOIN cities
-ON users.citi_id = cities.id
-WHERE cities.name IN("Moscow");
+CREATE DATABASE Posts;
+
+CREATE TABLE `Posts`.`post` 
+(`id` INT NOT NULL AUTO_INCREMENT, 
+`userId` INT NOT NULL, 
+`title` VARCHAR(255) NOT NULL, 
+`body` TEXT NOT NULL, 
+PRIMARY KEY (`id`));
+
+CREATE TABLE `Posts`.`comment` 
+(`id` INT NOT NULL AUTO_INCREMENT , 
+ `name` VARCHAR(255) NOT NULL , 
+ `email` VARCHAR(255) NOT NULL , 
+ `body` TEXT NOT NULL , 
+ `postId` INT NOT NULL , 
+ PRIMARY KEY (`id`),
+ FOREIGN KEY (`postId`) REFERENCES `Posts`.`post`(id));
